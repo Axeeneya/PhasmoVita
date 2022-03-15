5378cb57f203a219e339937ba8c65af093ef5ac1
Максим Ковров <sataneshiko29@gmail.com>
2022-03-15 14:29:59 +0300

    Adding product to basket, Remove product from basket, Adding product to profile from basket

--- templates/footer.php
+++ templates/footer.php
@@ -1,13 +1,89 @@
+<?php
+
+if ( ! isset($_SESSION)) {
+    session_start();
+}
+?>
 <div id="openModal" class="modal">
     <div class="modal-dialog">
         <div class="modal-content">
-          	<div class="modal-header">
-            	<h3 class="modal-title">Корзина</h3>
-            	<a href="#close" title="Close" class="close">×</a>
-          	</div>
-          	<div class="modal-body">
-            	Содержимое модального окна...
-          	</div>
+            <div class="modal-header">
+                <h3 class="modal-title">Корзина</h3>
+                <a href="#close" title="Close" class="close">×</a>
+            </div>
+            <div class="modal-body">
+
+                <table>
+                    <tr>
+                        <td></td>
+                        <td><b>Наименование</b></td>
+                        <td><b>Количество</b></td>
+                        <td><b>Цена за шт.</b></td>
+                        <td><b>Итого</b></td>
+                        <td><b></b></td>
+                    </tr>
+                    <?php
+                    //подключаемся к БД и выбираем все данные о наших товарах
+                    $sql_m       = $link->query("SELECT * FROM `products`");
+                    $Sum         = 0;
+                    $add_product = $_SESSION['add_product'];
+                    //проверяем, что корзина не пуста иначе будет выходить ошибка
+                    if (isset($add_product)) {
+                        //пербераем массив с добавленными товарами и выбираем id товара
+                        foreach ($add_product as $key => $value) {
+                            $a      = $key;  //id товара
+                            $kol    = $_SESSION['add_product'][$a];
+                            $good_m = [];
+                            foreach ($sql_m as $product_m) {
+                                if ($product_m['id'] == $a) {
+                                    $good_m = $product_m;
+                                    break;
+                                }
+                            } ?>
+                            <tr>
+                                <td>
+                                    <?php
+                                         echo '<img style="width: 90% auto; height:60px" src="data:image/png;base64,'.base64_encode($good_m['image']).'" >'
+                                    ?>
+                                </td>
+                                <td><?php echo $good_m['name']; ?></td>
+                                
+                                <td>
+                                    <input type="number" step="1" min="1"
+                                           max="10" id="num_count"
+                                           name="quantity"
+                                           value="<?php echo $kol; ?>"
+                                           title="Qty">
+                                </td>
+
+                                <td><?php if (!isset($good_m['discount'])){
+                                    echo $good_m['price'];
+                                    $realPrice = $good_m['price'];
+                                } else {
+                                   $realPrice = $good_m['price'] - ($good_m['price'] * $good_m['discount'])/100;
+                                   echo '<a class="oldPrice">'.$good_m['price']."</a>";
+                                   echo $realPrice; 
+                                }?></td>
+                                <td><?php echo $kol * $realPrice
+                                        . 'р'; ?></td>
+                                <td><b><a href="rem_basket.php?remId=<?php echo $a;?>">удалить</a></b></td>
+                            </tr>
+                            <?php
+                            $Sum += $kol * $realPrice;
+                        }
+                    }
+                    ?>
+                    <tr>
+                        <td align="right" colspan="5"><b> <?php echo 'Всего: '
+                                    . $Sum ?></b></td>
+                    </tr>
+                    <tr>
+                        <td align="right" colspan="5"><b> <a
+                                        href="add_basket.php">Оформить заказ</a></b>
+                        </td>
+                    </tr>
+                </table>
+            </div>
         </div>
     </div>
 </div>
--- templates/openProduct.php
+++ templates/openProduct.php
@@ -8,66 +8,102 @@
         <div class="blockTovarChild">
             <div class="topBlock">
                 <div class="leftTopBlock">
-                   <?php  echo '<img  src="data:image/png;base64,' .  base64_encode($good['image'])  . '" />'; ?>
-                </div>
-                <div class="rightTopBlock">
-                    <div class="<?php
-                        if (!isset($good['discount'])) {
-                            echo 'priceBlock';
-                        } else {
-                            echo 'priceBlockDiscount';
-                        }
+                 <?php  echo '<img  src="data:image/png;base64,' .  base64_encode($good['image'])  . '" />'; ?>
+             </div>
+             <div class="rightTopBlock">
+                <div class="<?php
+                if (!isset($good['discount'])) {
+                    echo 'priceBlock';
+                    } else {
+                        echo 'priceBlockDiscount';
+                    }
                     ?>">
                     <?php  
-                        if (!isset($good['discount'])) {
-                            require ('templates/price/withoutDiscount.php');
-                        } else {
+                    if (!isset($good['discount'])) {
+                        require ('templates/price/withoutDiscount.php');
+                    } else {
                         require ('templates/price/withDiscount.php');
-                        }
-                   ?>
-                    </div>
-                    <div class="cuntMan">
-                        <table class="tableCountMan">
-                            <tr>
-                                <td class="tabCoun">
-                                    Страна:  
-                                </td>
-                                <td>
-                                <?php
-                                    echo $good['country'];
-                                ?>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td class="tabMan">
-                                    Производитель: 
-                                </td>
-                                <td>
-                                    <?php
-                                        echo $good['Manufacturer'];
-                                    ?>
-                                </td>
-                            </tr>
-                        </table>
-                    </div>
-                </div>
-            </div>  
-        </div>  
-    </div>
-    <div class="blockTovar">
-        <div class="blockTovarChild">
-            <div class="bottomBlock">
-                <div class="topBottomBlock">
-                    <h1>Описание</h1>
-                </div>
-                <div class="bottomBottomBlock">
-                    <?php
-                        $desc=nl2br($good['desc']);
-                        echo $desc;
+                    }
                     ?>
                 </div>
+                <div class="cuntMan">
+                    <table class="tableCountMan">
+                        <tr>
+                            <td class="tabCoun">
+                                Страна:  
+                            </td>
+                            <td>
+                                <?php
+                                echo $good['country'];
+                                ?>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="tabMan">
+                                Производитель: 
+                            </td>
+                            <td>
+                                <?php
+                                echo $good['Manufacturer'];
+                                ?>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+                <form id="form1" name="form1" action="add_cart.php" method="post">
+
+                    <div class="input-group quantity_goods">
+                        <input type="button" value="-" id="button_minus">
+                        <input onchange="location=value" type="number" step="1" min="1"
+                        max="10" id="num_count" name="quantity" value="1"
+                        title="Qty">
+                        <input type="button" value="+" id="button_plus">
+                    </div>
+                    <!-- начало невидимой части формы -->
+                    <input type="hidden" name="product_id"
+                    value="<?php echo $good['id'] ?>"/>
+
+                    <!-- конец невидимой части формы -->
+
+                    <input class='add_to_cart' type="submit" value="В корзину"
+                    name="submit">
+
+                </form>
+            </div>
+        </div>  
+    </div>  
+</div>
+<div class="blockTovar">
+    <div class="blockTovarChild">
+        <div class="bottomBlock">
+            <div class="topBottomBlock">
+                <h1>Описание</h1>
+            </div>
+            <div class="bottomBottomBlock">
+                <?php
+                $desc=nl2br($good['desc']);
+                echo $desc;
+                ?>
             </div>
         </div>
     </div>
 </div>
+</div>
 
+    <script>
+        const numCount = document.getElementById('num_count');
+        const plusBtn = document.getElementById('button_plus');
+        const minusBtn = document.getElementById('button_minus');
+        plusBtn.onclick = function () {
+            let qty = parseInt(numCount.value);
+            qty = qty + 1;
+            numCount.value = qty;
+        }
+        minusBtn.onclick = function () {
+            let qty = parseInt(numCount.value);
+            if (qty > 1) {
+                qty = qty - 1;
+            }
+            numCount.value = qty;
+        }
+    </script>
